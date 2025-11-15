27ce4297efa3edde3b16794876a57bf08c4e31c8
lazizjon <suyunovlaziz1997@gmail.com>
2025-11-15 12:46:27 +0100

    json files added

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,14 +1,41 @@
 package it.roma.sapienza
 
+import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.plugins.calllogging.*
-import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import org.slf4j.event.*
 
 fun Application.configureRouting() {
+
+    val profileJson = environment.classLoader
+        .getResource("questionnaires/theBIKEnet_profile.json")
+        ?.readText()
+        ?: error("Failed to load profile survey questionnaires ")
+
+    val preTripJson = environment.classLoader
+        .getResource("questionnaires/theBIKEnet_trip_pre.json")
+        ?.readText()
+        ?: error("Failed to load pre trip survey questionnaires ")
+
+    val postTripJson = environment.classLoader
+        .getResource("questionnaires/theBIKEnet_trip_post.json")
+        ?.readText()
+        ?: error("Failed to load post trip survey questionnaires ")
+
     routing {
+
+        get("questionnaires/profile") {
+            call.respondText(profileJson, contentType = ContentType.Application.Json)
+        }
+
+        get("questionnaires/pre_trip") {
+            call.respondText(preTripJson, contentType = ContentType.Application.Json)
+        }
+
+        get("questionnaires/post_trip") {
+            call.respondText(postTripJson, contentType = ContentType.Application.Json)
+        }
+
         get("/") {
             call.respondText("Hello World!")
         }
