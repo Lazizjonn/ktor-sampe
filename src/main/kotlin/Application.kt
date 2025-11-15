16e87b0a1684a32fb4510ec327a1969d5eff87e0
lazizjon <suyunovlaziz1997@gmail.com>
2025-11-15 12:19:04 +0100

    merge with remote

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,14 @@
+package it.roma.sapienza
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureMonitoring()
+    configureSecurity()
+    configureRouting()
+}
